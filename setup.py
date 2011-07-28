8bab39a04726d8a9217e1004cc8d8dc943a19e7f
rw <robert.winslow@gmail.com>
2011-07-28 01:21:18 -0400

    first setup.py file

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Plainsight',
+      version='1.0',
+      description='A textual steganography tool.',
+      author='Robert Winslow',
+      author_email='robert.winslow@gmail.com',
+      url='http://github.com/rw/plainsight',
+      packages=['bitstring', 'progressbar'],
+     )
+
