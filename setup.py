c3971d965fdb5ead23b30c884c3cea8ab18ee57f
rw <robert.winslow@gmail.com>
2011-07-30 23:26:44 -0400

    more metadata to setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,22 @@
 #!/usr/bin/env python
 
 from setuptools import setup
-from sys import version
-
 
 setup(name='Plainsight',
-      version='1.0',
-      description='A textual steganography tool.',
+      version='1.0a',
+      description='A textual steganography tool to defeat censorship.',
       author='Robert Winslow',
       author_email='robert.winslow@gmail.com',
       url='http://github.com/rw/plainsight',
+      classifiers=['Development Status :: 4 - Beta',
+                   'Intended Audience :: Developers',
+                   'Intended Audience :: End Users/Desktop',
+                   'Natural Language :: English',
+                   'Programming Language :: Python',
+                   'Topic :: Security :: Cryptography'
+                   'Topic :: Text Processing :: Filters',
+                   'Topic :: Text Processing :: Linguistic',
+                  ],
       requires=['argparse', 'bitstring', 'progressbar'],
      )
 
